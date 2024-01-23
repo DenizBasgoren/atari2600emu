6d8b04fbc0efa2bb0b98ffd40a914bcba0502a5c
Deniz Basgoren <denizbasgoren1997@gmail.com>
2024-01-23 16:38:16 +0000

    Added load and store fns

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ let fileReader = new FileReader()
 let cartridgeBytes = null
 
 cartridgeReaderEl.onchange = ev => {
-    fileReader.readAsArrayBuffer( cartridgeReaderEl.files[0] )
+  fileReader.readAsArrayBuffer( cartridgeReaderEl.files[0] )
 }
 
 fileReader.onloadend = () => {
@@ -26,6 +26,8 @@ let flagD=0
 let flagB=0
 let flagV=0
 let flagN=0
+
+let RamBytes = new Uint8Array(128) // TODO init 0 or garbage?
 
 let /*00*/      VSYNC   // ......1.  vertical sync set-clear
 let /*01*/      VBLANK  // 11....1.  vertical blank set-clear
@@ -97,3 +99,315 @@ let /*0294*/    TIM1T   // 11111111  set 1 clock interval (838 nsec/interval)
 let /*0295*/    TIM8T   // 11111111  set 8 clock interval (6.7 usec/interval)
 let /*0296*/    TIM64T  // 11111111  set 64 clock interval (53.6 usec/interval)
 let /*0297*/    T1024T  // 01111111  set 1024 clock interval (858.2 usec/interval)
+
+
+let currentInst = {opcode: 'NOP', addressingMode: 'Implied', operand: 0}
+let executeAfter = 1
+
+/* Instruction::
+opcode: TAY TAX TSX TYA TXA TXS LDA LDX LDY STA STX STY PHA PHP PLA PLP ADC SBC AND EOR ORA CMP CPX CPY BIT INC INX INY DEC DEX DEY ASL LSR ROL ROR JMP JSR RTI RTS BPL BMI BVC BVS BCC BLT BCS BGE BNE BZC BEQ BZS BRK CLC CLI CLD CLV SEC SEI SED NOP
+addressingMode: Implied Immediate ZeroPage ZeroPageX ZeroPageY Absolute AbsoluteX AbsoluteY IndirectX IndirectY
+operand: (number)
+*/
+
+
+setInterval(() => {
+  for (let colorClock = 0; colorClock<262*228; colorClock++) {
+    if (colorClock%3==0) {
+      executeAfter--
+      if (executeAfter==0) {
+        executeInst()
+      }
+    }
+  }
+}, 1/60);
+
+function executeInst() {
+
+  if (opcode=='TAY') {
+    regY = regA
+  }
+  else if (opcode=='TAX') {
+    regX = regA
+  }
+  else if (opcode=='TSX') {
+    regX = regS
+  }
+  else if (opcode=='TYA') {
+    regA = regY
+  }
+  else if (opcode=='TXA') {
+    regA = regX
+  }
+  else if (opcode=='TXS') {
+    regS = regX
+  }
+  else if (opcode=='LDA') {
+    regA = loadValueUsingInst(currentInst)
+  }
+  else if (opcode=='LDX') {
+    regX = loadValueUsingInst(currentInst)
+  }
+  else if (opcode=='LDY') {
+    regY = loadValueUsingInst(currentInst)
+  }
+  else if (opcode=='STA') {
+    storeValueUsingInst(currentInst, regA)
+  }
+  else if (opcode=='STX') {
+    storeValueUsingInst(currentInst, regX)
+  }
+  else if (opcode=='STY') {
+    storeValueUsingInst(currentInst, regY)
+  }
+  else if (opcode=='PHA') {
+
+  }
+  else if (opcode=='PHP') {
+
+  }
+  else if (opcode=='PLA') {
+
+  }
+  else if (opcode=='PLP') {
+
+  }
+  else if (opcode=='ADC') {
+    regA += loadValueUsingInst(currentInst) + flagC
+    regA = normalize(regA, 8)
+  }
+  else if (opcode=='SBC') {
+    regA += flagC - 1 - loadValueUsingInst(currentInst)
+    regA = normalize(regA, 8)
+  }
+  else if (opcode=='AND') {
+    regA &= loadValueUsingInst(currentInst)
+    regA = normalize(regA, 8)
+  }
+  else if (opcode=='EOR') {
+    regA ^= loadValueUsingInst(currentInst)
+    regA = normalize(regA, 8)
+  }
+  else if (opcode=='ORA') {
+    regA |= loadValueUsingInst(currentInst)
+    regA = normalize(regA, 8)
+  }
+  else if (opcode=='CMP') {
+    /////
+  }
+  else if (opcode=='CPX') {
+
+  }
+  else if (opcode=='CPY') {
+
+  }
+  else if (opcode=='BIT') {
+
+  }
+  else if (opcode=='INC') {
+
+  }
+  else if (opcode=='INX') {
+
+  }
+  else if (opcode=='INY') {
+
+  }
+  else if (opcode=='DEC') {
+
+  }
+  else if (opcode=='DEX') {
+
+  }
+  else if (opcode=='DEY') {
+
+  }
+  else if (opcode=='ASL') {
+
+  }
+  else if (opcode=='LSR') {
+
+  }
+  else if (opcode=='ROL') {
+
+  }
+  else if (opcode=='ROR') {
+
+  }
+  else if (opcode=='JMP') {
+
+  }
+  else if (opcode=='JSR') {
+
+  }
+  else if (opcode=='RTI') {
+
+  }
+  else if (opcode=='RTS') {
+
+  }
+  else if (opcode=='BPL') {
+
+  }
+  else if (opcode=='BMI') {
+
+  }
+  else if (opcode=='BVC') {
+
+  }
+  else if (opcode=='BVS') {
+
+  }
+  else if (opcode=='BCC') {
+
+  }
+  else if (opcode=='BLT') {
+
+  }
+  else if (opcode=='BCS') {
+
+  }
+  else if (opcode=='BGE') {
+
+  }
+  else if (opcode=='BNE') {
+
+  }
+  else if (opcode=='BZC') {
+
+  }
+  else if (opcode=='BEQ') {
+
+  }
+  else if (opcode=='BZS') {
+
+  }
+  else if (opcode=='BRK') {
+
+  }
+  else if (opcode=='CLC') {
+
+  }
+  else if (opcode=='CLI') {
+
+  }
+  else if (opcode=='CLD') {
+
+  }
+  else if (opcode=='CLV') {
+
+  }
+  else if (opcode=='SEC') {
+
+  }
+  else if (opcode=='SEI') {
+
+  }
+  else if (opcode=='SED') {
+
+  }
+  else if (opcode=='NOP') {
+
+  }
+  
+}
+
+function getByteFromMemory(addr, triggerIO) {
+
+}
+
+function setByteToMemory(addr, value, triggerIO) {
+
+}
+
+function loadValueUsingInst(inst) {
+  if (inst.addressingMode=='Immediate') {
+    return inst.operand
+  }
+  else if (inst.addressingMode=='ZeroPage') {
+    return getByteFromMemory(inst.operand, true)
+  }
+  else if (inst.addressingMode=='ZeroPageX') {
+    return getByteFromMemory((inst.operand+regX)%256, true)
+  }
+  else if (inst.addressingMode=='ZeroPageY') {
+    return getByteFromMemory((inst.operand+regY)%256, true)
+  }
+  else if (inst.addressingMode=='Absolute') {
+    return getByteFromMemory(inst.operand, true)
+  }
+  else if (inst.addressingMode=='AbsoluteX') {
+    if (inst.operand%256+regX >= 256) {
+      getByteFromMemory(inst.operand+regX-256, true)
+    }
+    return getByteFromMemory(inst.operand+regX, true)
+  }
+  else if (inst.addressingMode=='AbsoluteY') {
+    if (inst.operand%256+regY >= 256) {
+      getByteFromMemory(inst.operand+regY-256, true)
+    }
+    return getByteFromMemory(inst.operand+regY, true)
+  }
+  else if (inst.addressingMode=='IndirectX') {
+    let addr = getByteFromMemory((inst.operand+regX)%256, false) +
+      getByteFromMemory((inst.operand+regX)%256+1, false) << 8
+    return getByteFromMemory(addr, true)
+  }
+  else if (inst.addressingMode=='IndirectY') {
+    let addr = getByteFromMemory(inst.operand, false) +
+      getByteFromMemory(inst.operand+1, false) << 8
+    if (addr%256+regY >= 256) {
+      getByteFromMemory(addr+regY-256, true)
+    }
+    return getByteFromMemory(addr+regY, true)
+  }
+  else {
+    unreachable()
+  }
+}
+
+function storeValueUsingInst(inst, value) {
+  if (inst.addressingMode=='Immediate') {
+    unreachable()
+  }
+  else if (inst.addressingMode=='ZeroPage') {
+    setByteToMemory(inst.operand, value, true)
+  }
+  else if (inst.addressingMode=='ZeroPageX') {
+    setByteToMemory((inst.operand+regX)%256, value, true)
+  }
+  else if (inst.addressingMode=='ZeroPageY') {
+    setByteToMemory((inst.operand+regY)%256, value, true)
+  }
+  else if (inst.addressingMode=='Absolute') {
+    setByteToMemory(inst.operand, value, true)
+  }
+  else if (inst.addressingMode=='AbsoluteX') {
+    setByteToMemory(inst.operand+regX, value, true)
+  }
+  else if (inst.addressingMode=='AbsoluteY') {
+    setByteToMemory(inst.operand+regY, value, true)
+  }
+  else if (inst.addressingMode=='IndirectX') {
+    let addr = getByteFromMemory((inst.operand+regX)%256, false) + // TODO: are these true or false?
+      getByteFromMemory((inst.operand+regX)%256+1, false) << 8
+    setByteToMemory(addr, value, true)
+  }
+  else if (inst.addressingMode=='IndirectY') {
+    let addr = getByteFromMemory(inst.operand, false) +
+      getByteFromMemory(inst.operand+1, false) << 8
+    setByteToMemory(addr+regY, value, true)
+  }
+  else {
+    unreachable()
+  }
+}
+
+function unreachable() {
+  console.error('unreachable')
+}
+
+function normalize(val, bitLength) {
+  let max = 2**bitLength
+  return (val % max + max) % max
+}
