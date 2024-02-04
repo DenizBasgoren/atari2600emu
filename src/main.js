59974e074de2a8eac91245610b4ee3eccdc8fcfb
Deniz Basgoren <denizbasgoren1997@gmail.com>
2024-02-04 08:18:12 +0300

    added boilerplate for io registers

--- src/main.js
+++ src/main.js
@@ -14,6 +14,10 @@ fileReader.onloadend = () => {
   cartridgeBytes = new Uint8Array(fileReader.result) 
 }
 
+let tvEl = document.querySelector('#tv')
+let tvCtx = tvEl.getContext('2d')
+
+
 let regA=0 // 8bit
 let regX=0 // 8bit
 let regY=0 // 8bit
@@ -40,79 +44,6 @@ function setRegP(value) {
   flagZ = (value & (1 << 1)) !== 0
   flagC = (value & 1) !== 0
 }
-
-let RamBytes = new Uint8Array(128) // TODO init 0 or garbage?
-
-let /*00*/      VSYNC   // ......1.  vertical sync set-clear
-let /*01*/      VBLANK  // 11....1.  vertical blank set-clear
-let /*02*/      WSYNC   // <strobe>  wait for leading edge of horizontal blank
-let /*03*/      RSYNC   // <strobe>  reset horizontal sync counter
-let /*04*/      NUSIZ0  // ..111111  number-size player-missile 0
-let /*05*/      NUSIZ1  // ..111111  number-size player-missile 1
-let /*06*/      COLUP0  // 1111111.  color-lum player 0 and missile 0
-let /*07*/      COLUP1  // 1111111.  color-lum player 1 and missile 1
-let /*08*/      COLUPF  // 1111111.  color-lum playfield and ball
-let /*09*/      COLUBK  // 1111111.  color-lum background
-let /*0A*/      CTRLPF  // ..11.111  control playfield ball size & collisions
-let /*0B*/      REFP0   // ....1...  reflect player 0
-let /*0C*/      REFP1   // ....1...  reflect player 1
-let /*0D*/      PF0     // 1111....  playfield register byte 0
-let /*0E*/      PF1     // 11111111  playfield register byte 1
-let /*0F*/      PF2     // 11111111  playfield register byte 2
-let /*10*/      RESP0   // <strobe>  reset player 0
-let /*11*/      RESP1   // <strobe>  reset player 1
-let /*12*/      RESM0   // <strobe>  reset missile 0
-let /*13*/      RESM1   // <strobe>  reset missile 1
-let /*14*/      RESBL   // <strobe>  reset ball
-let /*15*/      AUDC0   // ....1111  audio control 0
-let /*16*/      AUDC1   // ....1111  audio control 1
-let /*17*/      AUDF0   // ...11111  audio frequency 0
-let /*18*/      AUDF1   // ...11111  audio frequency 1
-let /*19*/      AUDV0   // ....1111  audio volume 0
-let /*1A*/      AUDV1   // ....1111  audio volume 1
-let /*1B*/      GRP0    // 11111111  graphics player 0
-let /*1C*/      GRP1    // 11111111  graphics player 1
-let /*1D*/      ENAM0   // ......1.  graphics (enable) missile 0
-let /*1E*/      ENAM1   // ......1.  graphics (enable) missile 1
-let /*1F*/      ENABL   // ......1.  graphics (enable) ball
-let /*20*/      HMP0    // 1111....  horizontal motion player 0
-let /*21*/      HMP1    // 1111....  horizontal motion player 1
-let /*22*/      HMM0    // 1111....  horizontal motion missile 0
-let /*23*/      HMM1    // 1111....  horizontal motion missile 1
-let /*24*/      HMBL    // 1111....  horizontal motion ball
-let /*25*/      VDELP0  // .......1  vertical delay player 0
-let /*26*/      VDELP1  // .......1  vertical delay player 1
-let /*27*/      VDELBL  // .......1  vertical delay ball
-let /*28*/      RESMP0  // ......1.  reset missile 0 to player 0
-let /*29*/      RESMP1  // ......1.  reset missile 1 to player 1
-let /*2A*/      HMOVE   // <strobe>  apply horizontal motion
-let /*2B*/      HMCLR   // <strobe>  clear horizontal motion registers
-let /*2C*/      CXCLR   // <strobe>  clear collision latches
-let /*30*/      CXM0P   // 11......  read collision M0-P1, M0-P0 (Bit 7,6)
-let /*31*/      CXM1P   // 11......  read collision M1-P0, M1-P1
-let /*32*/      CXP0FB  // 11......  read collision P0-PF, P0-BL
-let /*33*/      CXP1FB  // 11......  read collision P1-PF, P1-BL
-let /*34*/      CXM0FB  // 11......  read collision M0-PF, M0-BL
-let /*35*/      CXM1FB  // 11......  read collision M1-PF, M1-BL
-let /*36*/      CXBLPF  // 1.......  read collision BL-PF, unused
-let /*37*/      CXPPMM  // 11......  read collision P0-P1, M0-M1
-let /*38*/      INPT0   // 1.......  read pot port
-let /*39*/      INPT1   // 1.......  read pot port
-let /*3A*/      INPT2   // 1.......  read pot port
-let /*3B*/      INPT3   // 1.......  read pot port
-let /*3C*/      INPT4   // 1.......  read input
-let /*3D*/      INPT5   // 1.......  read input
-let /*80..FF*/  RAM     // 11111111  128 bytes RAM (in PIA chip) for variables and stack
-let /*0280*/    SWCHA   // 11111111  Port A; input or output  (read or write)
-let /*0281*/    SWACNT  // 11111111  Port A DDR, 0= input, 1=output
-let /*0282*/    SWCHB   // 11111111  Port B; console switches (read only)
-let /*0283*/    SWBCNT  // 11111111  Port B DDR (hardwired as input)
-let /*0284*/    INTIM   // 11111111  Timer output (read only)
-let /*0285*/    INSTAT  // 11......  Timer Status (read only, undocumented)
-let /*0294*/    TIM1T   // 11111111  set 1 clock interval (838 nsec/interval)
-let /*0295*/    TIM8T   // 11111111  set 8 clock interval (6.7 usec/interval)
-let /*0296*/    TIM64T  // 11111111  set 64 clock interval (53.6 usec/interval)
-let /*0297*/    T1024T  // 01111111  set 1024 clock interval (858.2 usec/interval)
 
 
 let currentInst = {opcode: 'NOP', addressingMode: 'Implied', operand: 0}
@@ -457,12 +388,294 @@ function executeInst() {
   
 }
 
-function getByteFromMemory(addr, triggerIO) {
+let ram = Array(128).map(v => 0) // TODO init 0 or garbage?
+let electronBeamX = 0
+let electronBeamY = 0
+let vsync = 0 // 0=off 1=on
+let vblank = 0 // 0=off 1=on
 
+
+function getByteFromMemory(addr, triggerIO) {
+  // TODO handle 0x3F here
+
+  if (addr & 0x1000) {
+    // cartridge
+  }
+  else {
+    if (addr & 0x0080) {
+      // PIA
+      if (addr & 0x0200) {
+        // PIA registers
+        let regNo = addr & 0x0007
+        if (regNo==0) {
+          // SWCHA
+        }
+        else if (regNo==1) {
+          // SWACNT
+        }
+        else if (regNo==2) {
+          // SWCHB
+        }
+        else if (regNo==3) {
+          // SWBCNT
+        }
+        else if (regNo==4 || regNo==6) {
+          // INTIM
+        }
+        else if (regNo==5 || regNo==7) {
+          // INSTAT
+        }
+      }
+      else {
+        // PIA RAM
+        return ram[addr & 0x007F]
+      }
+    }
+    else {
+      // TIA
+      let regNo = addr & 0x000F
+      if (regNo==0) {
+        // CXM0P
+      }
+      else if (regNo==1) {
+        // CXM1P
+      }
+      else if (regNo==2) {
+        // CXP0FB
+      }
+      else if (regNo==3) {
+        // CXP1FB
+      }
+      else if (regNo==4) {
+        // CXM0FB
+      }
+      else if (regNo==5) {
+        // CXM1FB
+      }
+      else if (regNo==6) {
+        // CXBLPF
+      }
+      else if (regNo==7) {
+        // CXPPMM
+      }
+      else if (regNo==8) {
+        // INPT0
+      }
+      else if (regNo==9) {
+        // INPT1
+      }
+      else if (regNo==10) {
+        // INPT2
+      }
+      else if (regNo==11) {
+        // INPT3
+      }
+      else if (regNo==12) {
+        // INPT4
+      }
+      else if (regNo==13) {
+        // INPT5
+      }
+      else return readAddrNotMapped(addr)
+    }
+  }
+}
+
+function readAddrNotMapped(addr) {
+  return 0 // TODO or garbage?
 }
 
 function setByteToMemory(addr, value, triggerIO) {
+  if (addr & 0x1000) {
+    // cartridge
+  }
+  else {
+    if (addr & 0x0080) {
+      // PIA
+      if (addr & 0x0200) {
+        // PIA registers
+        let regNo = addr & 0x0007
+        if (regNo==0) {
+          // SWCHA
+        }
+        else if (regNo==1) {
+          // SWACNT
+        }
+        else if (regNo==2) {
+          // SWCHB
+        }
+        else if (regNo==3) {
+          // SWBCNT
+        }
+        else if (regNo==4) {
+          // TIM1T
+        }
+        else if (regNo==5) {
+          // TIM8T
+        }
+        else if (regNo==6) {
+          // TIM64T
+        }
+        else if (regNo==7) {
+          // T1024T
+        }
+      }
+      else {
+        // PIA RAM
+        ram[addr & 0x007F] = value
+      }
+    }
+    else {
+      // TIA
+      let regNo = addr & 0x003F
+      if (regNo==0x00) {
+        // VSYNC
+        let newVsync = (value & 2) >> 1
+        if (vsync==1 && newVsync==0) {
+          tvCtx.clearRect(0,0,500,500)
+        }
+        vsync = newVsync
+      }
+      else if (regNo==0x01) {
+        // VBLANK
+        vblank = (value & 2) >> 1
+      }
+      else if (regNo==0x02) {
+        // WSYNC
+      }
+      else if (regNo==0x03) {
+        // RSYNC
+      }
+      else if (regNo==0x04) {
+        // NUSIZ0
+      }
+      else if (regNo==0x05) {
+        // NUSIZ1
+      }
+      else if (regNo==0x06) {
+        // COLUP0
+      }
+      else if (regNo==0x07) {
+        // COLUP1
+      }
+      else if (regNo==0x08) {
+        // COLUPF
+      }
+      else if (regNo==0x09) {
+        // COLUBK
+      }
+      else if (regNo==0x0A) {
+        // CTRLPF
+      }
+      else if (regNo==0x0B) {
+        // REFP0
+      }
+      else if (regNo==0x0C) {
+        // REFP1
+      }
+      else if (regNo==0x0D) {
+        // PF0
+      }
+      else if (regNo==0x0E) {
+        // PF1
+      }
+      else if (regNo==0x0F) {
+        // PF2
+      }
+      else if (regNo==0x10) {
+        // RESP0
+      }
+      else if (regNo==0x11) {
+        // RESP1
+      }
+      else if (regNo==0x12) {
+        // RESM0
+      }
+      else if (regNo==0x13) {
+        // RESM1
+      }
+      else if (regNo==0x14) {
+        // RESBL
+      }
+      else if (regNo==0x15) {
+        // AUDC0
+      }
+      else if (regNo==0x16) {
+        // AUDC1
+      }
+      else if (regNo==0x17) {
+        // AUDF0
+      }
+      else if (regNo==0x18) {
+        // AUDF1
+      }
+      else if (regNo==0x19) {
+        // AUDV0
+      }
+      else if (regNo==0x1A) {
+        // AUDV1
+      }
+      else if (regNo==0x1B) {
+        // GRP0
+      }
+      else if (regNo==0x1C) {
+        // GRP1
+      }
+      else if (regNo==0x1D) {
+        // ENAM0
+      }
+      else if (regNo==0x1E) {
+        // ENAM1
+      }
+      else if (regNo==0x1F) {
+        // ENABL
+      }
+      else if (regNo==0x20) {
+        // HMP0
+      }
+      else if (regNo==0x21) {
+        // HMP1
+      }
+      else if (regNo==0x22) {
+        // HMM0
+      }
+      else if (regNo==0x23) {
+        // HMM1
+      }
+      else if (regNo==0x24) {
+        // HMBL
+      }
+      else if (regNo==0x25) {
+        // VDELP0
+      }
+      else if (regNo==0x26) {
+        // VDELP1
+      }
+      else if (regNo==0x27) {
+        // VDELBL
+      }
+      else if (regNo==0x28) {
+        // RESMP0
+      }
+      else if (regNo==0x29) {
+        // RESMP1
+      }
+      else if (regNo==0x2A) {
+        // HMOVE
+      }
+      else if (regNo==0x2B) {
+        // HMCLR
+      }
+      else if (regNo==0x2C) {
+        // CXCLR
+      }
+      else writeAddrNotMapped(addr, value)
+    }
+  }
+}
 
+function writeAddrNotMapped(addr, value) {
+  // nop
 }
 
 function loadValueUsingInst(inst) {
@@ -831,3 +1044,31 @@ function shouldBranch(opcode) {
 // dummy write
 // INC, DEC, shift, rotates
 // - we assume dummy write is same as final write, so we just do store twice
+
+
+let recognizedCartridgeMappings = [
+  // atari 4k
+  [
+    // {
+    //   type: 'rom',
+    //   startOffset: 0
+    //   endOffset: 0xFFFF
+    // }
+  ]
+]
+
+/*
+type: 'rom'
+startOffset: int
+endOffset: int
+mappedAddresses: []
+triggerAddress: int
+triggerValue(?): int
+isInitiallyMapped: bool
+
+type: 'ram'
+length: int
+mappedReadAddresses: []
+mappedWriteAddresses: []
+*/
+
